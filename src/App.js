da218642cf30d916c760e9d4e6247e5973c3c9c4
arayra <amandavianiva@outlook.com>
2021-10-26 03:30:45 +0700

    deleting Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import ExpenseItem from "./components/ExpenseItem";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <Navbar />
       <h2>Let's get started!</h2>
       <p>ini adalah react pertama arayra</p>
       <ExpenseItem />
