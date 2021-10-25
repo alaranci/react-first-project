08b81593b8bdcafa199683d37638840500629f1c
arayra <amandavianiva@outlook.com>
2021-10-26 02:16:18 +0700

    writing more complex jsx code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
+      <Navbar />
       <h2>Let's get started!</h2>
       <p>ini adalah react pertama arayra</p>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      ini adalah Navbar
+      <p>ini navbar wahahhaha</p>
+    </div>
+  );
+}
