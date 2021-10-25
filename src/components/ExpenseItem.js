e23fdd9c0054c20b0af1f4e6fa8a23076386cc2a
arayra <amandavianiva@outlook.com>
2021-10-26 03:28:13 +0700

    outputing dynamic data and working with expressions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
+  const expenseDate = new Date(1928, 10, 28).toISOString();
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
+  console.log(expenseDate);
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
