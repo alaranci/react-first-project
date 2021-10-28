51e43f95f434f1b185310c114b34fbc52dd5b825
arayra <amandavianiva@outlook.com>
2021-10-28 19:00:11 +0700

    how component functions are executed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,15 +4,16 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
+  let title = props.title;
   const clickHandler = () => {
-    console.log("clicked! " + props.id);
+    title = "Persija Jakarta";
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
